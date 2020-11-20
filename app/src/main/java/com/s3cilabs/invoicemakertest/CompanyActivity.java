3ba5ab7b0ec56445de42977c9cc2d5ad2635f178
06serseri <tunaalkan91@gmail.com>
2020-11-20 08:31:26 -0500

    Saved company name in shared preferences and restored to a local variable

--- app/src/main/java/com/s3cilabs/invoicemakertest/CompanyActivity.java
+++ app/src/main/java/com/s3cilabs/invoicemakertest/CompanyActivity.java
@@ -2,22 +2,29 @@ package com.s3cilabs.invoicemakertest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CompanyActivity extends AppCompatActivity {
     private static final String TAG = "CompanyActivity";
     TextView textViewCompanyName, textViewCompanyAddress, textViewCompanyPhone, textViewCompanyEmail,
-            textViewCompanyHst, textViewCompanyRemittance;
+            textViewCompanyHst, textViewCompanyRemittance, textViewTest;
     EditText editTextCompanyName, editTextCompanyAddress, editTextCompanyPhone, editTextCompanyEmail,
             editTextCompanyHst, editTextCompanyRemittance;
-    Button buttonSaveCompany;
+    Button buttonApplyCompany, buttonSaveCompany;
 
+    //Constants to save data to shared preferences
+    public static final String SHARED_PREFERENCES = "sharedPreferences";
+    public static final String COMPANY_NAME_TEXT = "companyNameText";
+
+    //Local variables to restore data from shared preferences
+    private String companyName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +32,45 @@ public class CompanyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_company);
         initViews();
 
-        buttonSaveCompany.setOnClickListener(new View.OnClickListener() {
+        buttonApplyCompany.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CompanyActivity.this, MainActivity.class);
-                startActivity(intent);
+                textViewTest.setText(editTextCompanyName.getText().toString());
+
+//                Intent intent = new Intent(CompanyActivity.this, MainActivity.class);
+//                startActivity(intent);
             }
         });
 
+        buttonSaveCompany.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+        loadData();
+        updateData();
+    }
+
+    //Save data to from test view to shared preferences
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //editor.putString(COMPANY_NAME_TEXT, textViewTest.getText().toString());
+        editor.putString(COMPANY_NAME_TEXT, editTextCompanyName.getText().toString());
+        editor.apply();
+        Toast.makeText(this, "Company information saved", Toast.LENGTH_SHORT).show();
+    }
+
+    //Get data from shared preferences to a local variable
+    public void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
+        companyName = sharedPreferences.getString(COMPANY_NAME_TEXT, "");
+    }
+
+    public void updateData(){
+        textViewTest.setText(companyName);
     }
 
     private void initViews() {
@@ -43,6 +81,7 @@ public class CompanyActivity extends AppCompatActivity {
         textViewCompanyEmail = findViewById(R.id.textViewCompanyEmail);
         textViewCompanyHst = findViewById(R.id.textViewCompanyHst);
         textViewCompanyRemittance = findViewById(R.id.textViewCompanyRemittance);
+        textViewTest = findViewById(R.id.textViewTest);
 
         editTextCompanyName = findViewById(R.id.editTextCompanyName);
         editTextCompanyAddress = findViewById(R.id.editTextCompanyAddress);
@@ -51,6 +90,7 @@ public class CompanyActivity extends AppCompatActivity {
         editTextCompanyHst = findViewById(R.id.editTextCompanyHst);
         editTextCompanyRemittance = findViewById(R.id.editTextCompanyRemittance);
 
+        buttonApplyCompany = findViewById(R.id.buttonApplyCompany);
         buttonSaveCompany = findViewById(R.id.buttonSaveCompany);
     }
 }
